5dface147468481035729420f5c3ca50b4d624fb
Bwiggemans <b.wiggemans@home.nl>
2022-05-16 12:17:13 +0200

    Initial commit

---
+++ src/main/java/nl/qnh/qforce/domain/qforce_project/QforceProjectApplication.java
@@ -0,0 +1,13 @@
+package nl.qnh.qforce.domain.qforce_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QforceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QforceProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/qnh/qforce/domain/qforce_project/QforceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.qnh.qforce.domain.qforce_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QforceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
